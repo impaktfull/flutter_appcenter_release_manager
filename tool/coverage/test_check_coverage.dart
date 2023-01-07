28f7471f5097664c9103515f562b4f380826d540
Koen Van Looveren <vanlooverenkoen@hotmail.com>
2023-01-07 12:37:14 +0100

    Updated the coverage checks

---
+++ tool/coverage/test_check_coverage.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+const minRequiredCoverage = 100;
+
+void main(List<String> args) {
+  printMessage('Start checking the lcov.info file');
+  final file = File('coverage/lcov.info');
+  if (!file.existsSync()) {
+    printMessage('${file.path}" does not exist');
+    return;
+  }
+  int totalLines = 0;
+  int totalLinesCovered = 0;
+  final lines = file.readAsLinesSync();
+  for (final line in lines) {
+    if (line.startsWith('DA')) {
+      totalLines++;
+    } else if (line.startsWith('LH')) {
+      totalLinesCovered += int.parse(line.replaceAll('LH:', ''));
+    }
+  }
+  final codeCoveragePercentage = (totalLinesCovered / totalLines) * 100;
+  if (codeCoveragePercentage >= minRequiredCoverage) {
+    printMessage('\n100% CODE COVERAGE!!!!\n');
+  } else {
+    printMessage('\nCODE COVERAGE IS TO LOW!!\n');
+    printMessage('COVERAGE IS ${codeCoveragePercentage.toStringAsFixed(2)}\n');
+    printMessage('AMOUNT OF LINES:$totalLines');
+    printMessage('AMOUNT OF COVERED:$totalLinesCovered\n');
+    exit(-1);
+  }
+}
+
+void printMessage(String message) {
+  // ignore: avoid_print
+  print(message);
+}
